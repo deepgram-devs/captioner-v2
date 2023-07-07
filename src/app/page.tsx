4e8847ef4d160c0165e731d88ea48b454d164604
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2023-07-07 15:33:29 -0400

    Replacing with the new version of Captioner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,115 @@
-import Image from 'next/image'
+// "use client";
+
+// import { Session, useSession } from "@supabase/auth-helpers-react";
+// import { useRouter } from "next/navigation";
+// import LoginLayout from "@/components/layouts/LoginLayout";
+// import LoginBox from "@/components/LoginBox";
+// import { BeakerIcon } from "@heroicons/react/24/outline";
+// import { Dialog, Transition } from "@headlessui/react";
+// import { Fragment, useState, useEffect } from "react";
+// import {cookies} from "next/headers"
+
+// const isEmpty = (obj: Session | null): boolean => {
+//   if (obj === null) return true;
+
+//   return Object.keys(obj).length === 0;
+// };
+
+// export default function Home() {
+
+//   let [open, setOpen] = useState(false);
+//   const router = useRouter();
+
+//   // if there is a session, get a user
+
+//   const session  = useSession();
+
+//   useEffect(() => {
+//     if (!isEmpty(session)) {
+//       router.push("/app");
+//     } else {
+//       setOpen(true);
+//     }
+//   }, [session]);
+
+//   return (
+//     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+//       <LoginLayout>
+//       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 sm:mx-auto sm:w-full sm:max-w-md">
+//         <div className="rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[0.125rem]">
+//           <div className="bg-black p-8 roußnded">
+//             <div className="flex flex-col items-center">
+//               <BeakerIcon className="h-20 w-20 stroke-red-500 animate-spin mb-4" />
+//               {isEmpty(session) ? (
+//                 <>
+//                   {open ? (
+//                     <span className="loading">Authenticating</span>
+//                   ) : (
+//                     <span className="loading">Loading</span>
+//                   )}
+//                 </>
+//               ) : (
+//                 <span className="loading">Signing in</span>
+//               )}
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <Transition appear show={open} as={Fragment}>
+//         <Dialog as="div" className="relative z-10" onClose={() => {}}>
+//           <Transition.Child
+//             as={Fragment}
+//             enter="ease-out duration-300"
+//             enterFrom="opacity-0"
+//             enterTo="opacity-100"
+//             leave="ease-in duration-200"
+//             leaveFrom="opacity-100"
+//             leaveTo="opacity-0"
+//           >
+//             <div className="fixed inset-0 bg-black bg-opacity-25" />
+//           </Transition.Child>
+
+//           <div className="fixed inset-0 overflow-y-auto">
+//             <div className="flex min-h-full items-center justify-center p-4 text-center">
+//               <Transition.Child
+//                 as={Fragment}
+//                 enter="ease-out duration-300"
+//                 enterFrom="opacity-0 scale-95"
+//                 enterTo="opacity-100 scale-100"
+//                 leave="ease-in duration-200"
+//                 leaveFrom="opacity-100 scale-100"
+//                 leaveTo="opacity-0 scale-95"
+//               >
+//                 <Dialog.Panel className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 sm:mx-auto sm:w-full sm:max-w-md">
+//                   <div className="rounded-md bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 p-[0.125rem]">
+//                     <div className="bg-zinc-950 p-8 rounded min-h-[12rem] flex flex-col justify-center">
+//                       {session ? (
+//                         <>
+//                           <BeakerIcon className="h-20 w-20 stroke-red-500 animate-spin mb-4 self-center" />
+//                           <span className="loading">Fetching user</span>
+//                         </>
+//                       ) : (
+//                         <LoginBox />
+//                       )}
+//                     </div>
+//                   </div>
+//                 </Dialog.Panel>
+//               </Transition.Child>
+//             </div>
+//           </div>
+//         </Dialog>
+//       </Transition>
+//     </LoginLayout>
+//     </main>
+//   )
+// }
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h1>Home</h1>
+    </div>
   )
 }
---
+++ src/components/providers/supabase-auth-provider.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import { Profile } from "@/types/collections";
+import { Session, User } from "@supabase/supabase-js";
+import { useRouter } from "next/navigation";
+import { createContext, useContext, useEffect } from "react";
+import useSWR from "swr";
+import { useSupabase } from "./supabase-provider";
+interface ContextI {
+  user: User | undefined | null,
+  error: any;
+  isLoading: boolean;
+  mutate: any;
+  signOut: () => Promise<void>;
+  signInWithEmail: (email: string, password: string) => Promise<string | null>;
+  createAccount: (email: string, password: string) => Promise<string | null>;
+}
+const Context = createContext<ContextI>({
+  user: null,
+  error: null,
+  isLoading: true,
+  mutate: null,
+  signOut: async () => {},
+  signInWithEmail: async (email: string, password: string) => null,
+  createAccount: async (email: string, password: string) => null,
+});
+
+export default function SupabaseAuthProvider({
+  serverSession,
+  children,
+}: {
+  serverSession?: Session | null;
+  children: React.ReactNode;
+}) {
+  const { supabase } = useSupabase();
+  const router = useRouter();
+
+  // Get USER
+  const getUser = async () => {
+    const user = serverSession?.user ?? null;
+    console.log("here", serverSession);
+    console.log("user", user);
+    return user;
+  };
+
+  const {
+    data: user,
+    error,
+    isLoading,
+    mutate,
+  } = useSWR(serverSession ? "profile-context" : null, getUser);
+
+  // Sign Out
+  const signOut = async () => {
+    await supabase.auth.signOut();
+    router.push("/login");
+  };
+
+  // Sign-In with Email
+  const signInWithEmail = async (email: string, password: string) => {
+    const { error } = await supabase.auth.signInWithPassword({
+      email,
+      password,
+    });
+
+    if (error) {
+      return error.message;
+    }
+
+    return null;
+  };
+
+  // Sign-Up with Email
+  const createAccount = async (email: string, password: string) => {
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+
+    if (error) {
+      return error.message;
+    }
+
+    return null;
+  };
+
+  // Refresh the Page to Sync Server and Client
+  useEffect(() => {
+
+    console.log("serverSession", serverSession);
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((event, session) => {
+      if (session?.access_token !== serverSession?.access_token) {
+        router.refresh();
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [router, supabase, serverSession?.access_token]);
+
+  const exposed: ContextI = {
+    user,
+    error,
+    isLoading,
+    mutate,
+    signOut,
+    signInWithEmail,
+  };
+
+  return <Context.Provider value={exposed}>{children}</Context.Provider>;
+}
+
+export const useAuth = () => {
+  let context = useContext(Context);
+  if (context === undefined) {
+    throw new Error("useAuth must be used inside SupabaseAuthProvider");
+  } else {
+    return context;
+  }
+};
