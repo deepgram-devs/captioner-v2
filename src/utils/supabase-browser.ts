823771a63bca214c85663091646e90f7232988da
Saarth Shah <69341449+SaarthShah@users.noreply.github.com>
2023-07-11 12:29:31 -0400

    reinitialized project

--- src/utils/supabase-browser.ts
+++
@@ -1,6 +0,0 @@
-import { Database } from "../types/supabase";
-import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-
-export const createClient = () => {
-    return createClientComponentClient<Database>();
-}
